b6936409c34e84a2a53bcc56decbc6d8a57e1b4d
bmeatayi <bmeatayi@gmail.com>
2018-10-13 16:59:32 +0200

    minor changes in parsePacket.m

--- api/parsePacket.m
+++ api/parsePacket.m
@@ -7,36 +7,42 @@ function [output] = parsePacket(fid)
 %   Output:
 %       - fid: file/stream identifier
 %
-%
+%   Github page: https://github.com/Mentalab-hub/explorematlab/ 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 output = [];
+
 interruptWarning = 'Stream interrupted unexpectedly!';
 fletcherMismatchWarning = 'Fletcher mismatch!';
 pidUnexpectedWarning = 'Unexpected package ID: ';
+
 [pid,n] = fread(fid,1,'*ubit8');    % Read the package ID
 if n==0
     warning(interruptWarning);
+    output.type = 'end';
     return; % No data in the stream/file
 end
 output.cnt = fread(fid,1,'*ubit8');        % Counter of the package
 payload = fread(fid,1,'uint16');    % Number of bytes in the package
-timestamp = fread(fid,1,'uint32');  % Timestamp of the package
+output.timestamp = fread(fid,1,'uint32')/100;  % Timestamp of the package in second
 
 switch pid
     case 13         % Orientation package
         output.type = 'orn';
         output.orn = fread(fid,(payload-8)/2,'int16');
+        output.orn = output.orn .* [0.061, 0.061, 0.061, 8.750, 8.750, 8.750, 1.52, 1.52, 1.52]';
     
     case 19         % Environment package
         output.type = 'env';
         output.temperature = fread(fid,1,'*bit8');
-        output.light = fread(fid,1,'uint16');
-        output.battery = fread(fid,1,'uint16');
+        output.light = (1000/4095) * fread(fid,1,'uint16'); % In Lux
+        output.battery = (16.8/6.8) * (1.8/2457) * fread(fid,1,'uint16'); % In volts
     
     case {144, 146, 30, 62} % EEG package
         [temp,n] = fread(fid,(payload-8)/3,'*bit24');
         if n< ((payload-8)/3) %check if the package terminates in between
             warning(interruptWarning);
+            output.type = 'end';
             return;
         end
         if pid == 144  % Specify the number of channel and reference voltage
@@ -44,14 +50,19 @@ switch pid
             nChan = 5;      % 4 channels + 1 status
             vref = 2.4;
             nPacket = 33;
-        elseif pid == 62
-            output.type = 'eeg4';
-            nChan = 5;      % 4 channels
+        elseif pid == 146
+            output.type = 'eeg8';
+            nChan = 9;      % 8 channels + 1 status
             vref = 2.4;
             nPacket = [];
-        else
-            output.type = 'eeg8'
+        elseif pid == 30
+            output.type = 'eeg8';
             nChan = 9;      % 8 channels + 1 status
+            vref = 4.5;
+            nPacket = [];
+        elseif pid == 62
+            output.type = 'eeg8';
+            nChan = 8;      % 8 channels
             vref = 4.5;
             nPacket = [];
         end
@@ -60,10 +71,11 @@ switch pid
         %output.status = temp(1,:);    %save the status of data points
     case 27
         output.type = 'ts';
-        output.timestamp = fread(fid,(payload-8)/4,'uint32');
+        output.ts = fread(fid,(payload-8)/4,'uint32');
     otherwise
         warning([pidUnexpectedWarning pid])
         temp = fread(fid,payload-8,'*bit8'); % Read the payload
+        output.type = 'end';
 end
 
 
@@ -74,6 +86,7 @@ if n<4
 elseif((pid~=27)&&(fletcher(4) ~= 222)) || ((pid==27)&&(fletcher(4)~=255))
     disp(fletcher);
     warning(fletcherMismatchWarning)
+    output.type = 'end';
 end
 
 end
